3d9e3f7cb21074e62a2e8ce8c726df8e8c7b3343
Oliver Switzer <oliverswitzer@gmail.com>
2024-01-11 00:00:50 -0500

    First pass at displaying cards of listings.
    * For some reason tailwind selectors no longer work after installing material UI... I was following along with this guide: https://mui.com/material-ui/guides/interoperability/\#tailwind-css
    * It may be some weird incompatibility with also having to use Next.js. This is the guide I followed for that https://mui.com/material-ui/guides/nextjs/

--- rental-buddy/ui/src/app/page.tsx
+++ rental-buddy/ui/src/app/page.tsx
@@ -1,112 +1,56 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
+import { Paper, Grid, Card, CardContent, Typography } from '@mui/material';
+import Carousel from 'react-material-ui-carousel';
+import { Listing, Neighborhood } from 'rb-shared';
+
+// Randomized additional data for cards
+const cards: Listing[] = Array.from({ length: 10 }, (_, index) => ({
+  id: `UUID-${index}`,
+  url: new URL(`https://placekitten.com/800/400?image=${index}`),
+  address: `${index}24 Example St. Apt ${index}B`,
+  rent: 2000 + index * 100,
+  images: [`https://placekitten.com/800/400?image=${index}`, `https://placekitten.com/800/400?image=${index + 1}`],
+  neighborhood: Neighborhood.GREENPOINT,
+  bedroomCount: 1,
+  bathroomCount: 1,
+  hasBrokerFee: index % 2 === 0,
+  sqFt: 400 + index * 10
+}));
+
+function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex w-[800px] min-h-screen flex-col items-center justify-between p-24">
+      <Grid container spacing={4} style={{ padding: 24 }}>
+        {cards.map(card => (
+          <Grid item xs={12} sm={6} md={4} key={card.id}>
+            <Card>
+              <Carousel autoPlay={false}>
+                {card.images.map((imgSrc, i) => (
+                  <Paper key={`${card.id}-img-${i}`} elevation={0}>
+                    <img src={imgSrc} alt={`Image of ${card.address}`} style={{ width: '100%', height: 'auto' }} />
+                  </Paper>
+                ))}
+              </Carousel>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              <CardContent>
+                <Typography className="bg-red-500" variant="h5" component="div">
+                  {card.address}
+                </Typography>
+                <Typography color="textSecondary">
+                  Rent: ${card.rent} - {card.bedroomCount} Bed, {card.bathroomCount} Bath, {card.sqFt} sqft
+                </Typography>
+                <Typography color="textSecondary">
+                  Neighborhood: {card.neighborhood}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </main>
-  )
+  );
 }
+
+export default Home;
+
